e33af4cadc8e88acf75212d7022ab15dae6a68f5
Poyokos <Poyokos@users.noreply.github.com>
2018-06-11 20:36:09 -0400

    minor fixes

--- registro.php
+++ registro.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
-    <script src=js/bootstrap.js></script>
+    <script src="js/bootstrap.js"></script>
     <title>Registro</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
